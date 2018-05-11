b39da2a7e413f54ac6304021358c6c410e2b16a4
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-05-11 12:22:47 +0200

    Calendar view generation

--- client/src/app/Componentes/showCalendar/showCalendar.component.ts
+++ client/src/app/Componentes/showCalendar/showCalendar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { CalendarService } from "../../services/calendar.service";
+import * as moment from "moment";
+import "moment/locale/es";
 
 @Component({
   selector: "app-showCalendar",
@@ -7,11 +9,99 @@ import { CalendarService } from "../../services/calendar.service";
   styleUrls: ["./showCalendar.component.css"]
 })
 export class ShowCalendarComponent implements OnInit {
-  calendar: any;
+  calendar: Array<any>;
+  monthDays: number;
+  firstDayMonth: number;
+  firstDaySecondWeek: number;
+  firstDayThirdWeek: number;
+  firstDayFourthWeek: number;
+  firstDayFifthWeek: number;
+  firstDaySixthWeek: number;
+  date: any;
+  monthName: string;
+  year: string;
 
   constructor(public calendarService: CalendarService) {}
 
   ngOnInit() {
-    
+    this.date = moment();
+    this.createCalendar();
+  }
+
+  createCalendar() {
+    this.monthName = moment(this.date).format("MMMM");
+    this.year = moment(this.date).format("YYYY");
+    this.monthDays = moment(this.date)
+      .endOf("month")
+      .date();
+    this.calendar = new Array(this.monthDays);
+    this.firstDayMonth = moment(this.date)
+      .startOf("month")
+      .weekday();
+
+    // First week
+    this.calendar[0] = [];
+    let day = 1;
+    for (let i = 0; i < 7; i++) {
+      if (i < this.firstDayMonth) {
+        this.calendar[0].push(" ");
+      } else {
+        this.calendar[0].push(day);
+        day++;
+      }
+    }
+
+    this.firstDaySecondWeek = this.calendar[0][6] + 1;
+    // Second week
+    this.calendar[1] = [];
+    for (let i = 0; i < 7; i++) {
+      this.calendar[1].push(this.firstDaySecondWeek + i);
+    }
+
+    this.firstDayThirdWeek = this.calendar[1][6] + 1;
+    if (this.firstDayThirdWeek < this.monthDays) {
+      this.calendar[2] = [];
+      for (let i = 0; i < 7; i++) {
+        this.calendar[2].push(this.firstDayThirdWeek + i);
+      }
+    }
+
+    this.firstDayFourthWeek = this.calendar[2][6] + 1;
+    if (this.firstDayFourthWeek < this.monthDays) {
+      this.calendar[3] = [];
+      for (let i = 0; i < 7; i++) {
+        this.calendar[3].push(this.firstDayFourthWeek + i);
+      }
+    }
+
+    this.firstDayFifthWeek = this.calendar[3][6] + 1;
+    if (this.firstDayFifthWeek < this.monthDays) {
+      this.calendar[4] = [];
+      for (let i = 0; i < 7; i++) {
+        if (this.firstDayFifthWeek + i <= this.monthDays) {
+          this.calendar[4].push(this.firstDayFifthWeek + i);
+        }
+      }
+    }
+
+    this.firstDaySixthWeek = this.calendar[4][6] + 1;
+    if (this.firstDaySixthWeek < this.monthDays) {
+      this.calendar[5] = [];
+      for (let i = 0; i < 7; i++) {
+        if (this.firstDaySixthWeek + i <= this.monthDays) {
+          this.calendar[5].push(this.firstDaySixthWeek + i);
+        }
+      }
+    }
+  }
+
+  previousMonth() {
+    this.date.subtract(1, "month");
+    this.createCalendar();
+  }
+
+  nextMonth() {
+    this.date.add(1, "month");
+    this.createCalendar();
   }
 }
--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { SignUpComponent } from "./Componentes/sign-up/sign-up.component";
 import { HomeComponent } from "./Componentes/home/home.component";
+import { ShowCalendarComponent } from "./Componentes/showCalendar/showCalendar.component";
 
 
 export const routes: Routes = [
@@ -8,5 +9,6 @@ export const routes: Routes = [
   { path: 'signup', component: SignUpComponent},
   //{ path: 'login', component: AuthLoginComponent},
   { path: 'home', component: HomeComponent},
+  { path: 'calendar', component: ShowCalendarComponent}
   
 ]
