cb2f440e854f76bf8fdffcae077f7ec6079412d7
Mapi Carrillo <mp.carrillo@yahoo.es>
2018-05-15 19:44:01 +0200

    Refactorizando con Beltran II

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,7 @@ const Moment = require("moment");
 const MomentRange = require("moment-range");
 
 const moment = MomentRange.extendMoment(Moment);
-let _ = require("lodash")
+let _ = require("lodash");
 /* GET home page */
 //Buscador index
 
@@ -16,41 +16,46 @@ router.post("/", (req, res, next) => {
   const greenFreeBuddy = [];
   const redFreeBuddy = [];
   const yellowFreeBuddy = [];
-  const datesBuddy = []
+  const datesBuddy = [];
   const range = moment.range(startDay, endDay);
-  user
-    .find({ dogBuddy: true, pc })
+  /* user.find({ dogBuddy: true, pc })
     .then(buddies => {
       buddies.forEach(e => {
         calendar.find({ user: e._id }).then(calendar => {
-          console.log(calendar)
           if (calendar.length > 0) {
             calendar[0].closedDays.forEach(date => {
-              datesBuddy.push(range.contains(moment(date)))
-              let colorBuddy = _.uniq(datesBuddy)
-              
-               if (colorBuddy.length == 2){
-               if(yellowFreeBuddy.indexOf(e) == -1){
-                yellowFreeBuddy.push(e);
-               
+              datesBuddy.push(range.contains(moment(date)));
+              let colorBuddy = _.uniq(datesBuddy);
+
+              if (colorBuddy.length == 2) {
+                if (yellowFreeBuddy.indexOf(e) == -1) {
+                  yellowFreeBuddy.push(e);
+                }
+              } else if (colorBuddy.length == 1 && colorBuddy[0] == true) {
+                if (redFreeBuddy.indexOf(e) == -1) {
+                  redFreeBuddy.push(e);
+                }
+              } else if (colorBuddy.length == 1 && colorBuddy[0] == false) {
+                if (greenFreeBuddy.indexOf(e) == -1) {
+                  greenFreeBuddy.push(e);
+                }
               }
-             }else if(colorBuddy.length == 1 && colorBuddy[0] == true){
-              if(redFreeBuddy.indexOf(e) == -1){
-                redFreeBuddy.push(e);
-               
-              }
-             } else if(colorBuddy.length == 1 && colorBuddy[0] == false) {
-              if(greenFreeBuddy.indexOf(e) == -1){
-                greenFreeBuddy.push(e);
-                
-              }
-            }  
-          });
-        }
-        console.log(greenFreeBuddy)
-          res.status(200).json({green: greenFreeBuddy, yellow: yellowFreeBuddy, red: redFreeBuddy});
+            });
+          }
         });
       });
+      console.log(greenFreeBuddy, redFreeBuddy, yellowFreeBuddy);
+      res.status(200).json({
+          green: greenFreeBuddy,
+          yellow: yellowFreeBuddy,
+          red: redFreeBuddy
+        });
+    }) */
+    calendar.find()
+    .populate('user')
+    .then(calendars=> {
+      console.log(calendars)
+      res.status(200).json(calendars)
     })
     .catch(error => res.status(500).json({ message: error }));
 });
